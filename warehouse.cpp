cfaf0c346a87a4c5bacd1041455d917449faa4fd
Jtwoolbright <48270610+Jtwoolbright@users.noreply.github.com>
2019-08-25 13:58:08 -0700

    Update warehouse.cpp

--- warehouse.cpp
+++ warehouse.cpp
@@ -1,7 +1,6 @@
   /*
    *         Name: Josh Woolbright
    *         Date: 3/27/2019
-   *       Course: CS2560.01
    *  Description: This file contains the code for the warehouse
    *               header file.
    *
